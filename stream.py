25db69e952b1976cfd4fce90604babf9ee8130e7
NoneCoding <danieldebrito2105@gmail.com>
2022-08-05 16:12:32 -0300

    Busca informaçõe que o Sandro precisa :)

---
+++ stream.py
@@ -0,0 +1,24 @@
+import youtube_dl
+
+
+def stream(search):
+    """
+    Fetches necessary info to stream audio from youtube
+
+    """
+    options = {
+        "prefer_ffmpeg": True,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ],
+        "format": "best",
+        "noplaylist": True,
+        "default_search": "auto",
+    }
+    with youtube_dl.YoutubeDL(options) as ydl:
+        # Returns the url in the dict that extract_info got
+        return ydl.extract_info(search, download=False)["entries"]
