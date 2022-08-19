a98d0abac966725df8915151ce775de427df4559
NoneCoding <danieldebrito2105@gmail.com>
2022-08-19 18:23:09 -0300

    Stream

--- stream.py
+++ stream.py
@@ -8,16 +8,10 @@ def stream(search):
     """
     options = {
         "prefer_ffmpeg": True,
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }
-        ],
         "format": "best",
         "noplaylist": True,
         "default_search": "auto",
+        "age_limit": 20
     }
     with youtube_dl.YoutubeDL(options) as ydl:
         # Returns the url in the dict that extract_info got
